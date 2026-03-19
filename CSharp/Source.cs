2d1ff9d189e1ca972a6fbe9985c94c022f8a5ced
ChocoChocobo <sharonovjr@yahoo.com>
2026-03-19 13:54:55 +0300

    19.03 Обработка исключительных ситуаций

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -2,106 +2,53 @@
 using System.Diagnostics;
 
 namespace CSharp
-{
-	enum Rarity
-	{
-		Common = 0,
-		Uncommon = 1,
-		Epic = 2,
-		Legendary = 3,
-		Champion = 4
-	}
-
-    class Unit
-    {
-		public delegate string BattleCryDelegate();
-
-		private int x, y;
-		private int health, elixirCost, attackDamage;
-		private string unitName;
-		private Rarity rarity;
-
-		public Unit(int x, int y, int health, int elixirCost, int attackDamage, string unitName, Rarity rarity)
-		{
-			this.x = x;
-			this.y = y;
-			this.health = health;
-			this.elixirCost = elixirCost;
-			this.attackDamage = attackDamage;
-			this.unitName = unitName;
-			this.rarity = rarity;
-		}
-
-		// Функция передвижения юнита принимает в качестве параметра делегат, который возвращает фразу, который выкрикивает юнит
-		public void Move(BattleCryDelegate battleCryDelegate, int x, int y)
-		{
-            Console.WriteLine($"{unitName} передвигается и говорит: {battleCryDelegate.Invoke()}!");
-            this.x = x; 
-			this.y = y;
-		}
-
-		public void Attack(Unit other)
-		{
-			other.TakeDamage(attackDamage);
-		}
-
-		public void TakeDamage(int damage)
-		{
-			health -= damage;
-		}
-
-		public string Scream()
-		{
-			return "Hooog riiideeeer!";
-		}
-
-		public string Cry()
-		{
-			return "Snickers!";
-		}
-
-		// EventHandler - делегат!
-
-		// Синтаксис объявления делегата:
-		// delegate тип_возвращаемого_значения имя(параметры);
-
-		// Синтаксис переменной делегата:
-		// имя_делегата имя_переменной_делегата;
-
-		// Синтаксис присвоения делегату функции:
-		// имя_переменной_делегата = имя_функции;
-
-		// Синтаксис вызова функции через делегат:
-		// имя_переменной_делагата(параметры);
-
-		// Синтаксис присовения множества функций:
-		// имя_переменной_делагата += имя_функции1;
-		// имя_переменной_делагата += имя_функции2;
-
-		// Синтаксис удаления функции из делегата:
-		// имя_переменной_делагата -= имя_функции1;
-	}
-
+{	
 	class Source
     {	
+		// ? у типа данных сигнализирует о том, что значение может быть равно null
+		static int Divide(int[]? array, int i, int j)
+		{
+			return array[i] / array[j];
+		}
+
 		static void Main(string[] args)
         {
-			Unit hogRider = new Unit(0, 0, 2048, 4, 383, "Хог", Rarity.Uncommon);
+			int[]? array = null;
 
-			hogRider.Move(hogRider.Scream, 3, 3);
-
-            /*ProcessStartInfo processStartInfo = new ProcessStartInfo();
-			processStartInfo.Arguments = "hog_rider.wav";
-			processStartInfo.FileName = "cmd.exe";
-
-			Process process = new Process();
-			process.StartInfo = processStartInfo;
-			process.Start();*/
-		}
+			try
+			{
+                Console.WriteLine($"Деление выполнено: {Divide(array, 0, 1)}");
+			}
+			catch (DivideByZeroException zeroException)
+			{
+                Console.WriteLine(zeroException.Message);
+			}
+			catch (NullReferenceException nullException)
+			{
+                Console.WriteLine(nullException.Message);
+            }
+			catch (IndexOutOfRangeException indexException)
+			{
+                Console.WriteLine(indexException.Message);
+            }
+        }
 	}
 }
 
-//		Практика
-// 1. Создать поле делегата у класса Unit, которое бы в дальнейшем принимало на себя задачу по смене цвета карты юнита. Например, у хог райдера - оранжевый, у скелетиков - голубой.
-// 2. Создать функцию смены цвета юнита, которая бы принимала объект делегата
-// 3. Создать две функции с типом возвращаемого значения string, которые бы в зависимости от поля rarity в конструкции switch, возвращали строку с цветом юнита.
+// Exception:
+//		DivideByZeroException
+//		ArgumentOutOfRangeException
+//		IndexOutOfRangeException
+//		ArgumentException
+//		InvalidCastException
+//		NullReferenceException
+
+// Свойства класса Exception:
+// InnerException
+// Message
+// Source
+// StackTrace
+// TargetSite
+
+//		Практика 
+// Необходимо воспроизвести ошибки в той же самой программе оставшиеся блоки catch: IndexOutOfRangeException, DivideByZeroException
