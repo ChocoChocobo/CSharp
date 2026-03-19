2b5c2052674151d1066b70384112537ec958256b
ChocoChocobo <sharonovjr@yahoo.com>
2026-03-19 14:58:17 +0300

    19.03 Свой класс исключений

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -3,21 +3,43 @@ using System.Diagnostics;
 
 namespace CSharp
 {	
+	class DivideException : Exception
+	{
+		private int i, j;
+		public DivideException(int i, int j)
+		{
+			this.i = i;
+			this.j = j;
+		}
+
+		public override string Message
+		{
+			get
+			{
+				return $"Деление на 0 в дроби: {i} / {j}";
+			}
+		}
+	}
+
 	class Source
     {	
 		// ? у типа данных сигнализирует о том, что значение может быть равно null
 		static int Divide(int[]? array, int i, int j)
 		{
+			if (array[j] == 0)
+			{
+				throw new DivideException(array[i], array[j]);
+			}
 			return array[i] / array[j];
 		}
 
 		static void Main(string[] args)
         {
-			int[]? array = null;
+			int[]? array = { 1, 2, 3, 0 };
 
 			try
 			{
-                Console.WriteLine($"Деление выполнено: {Divide(array, 0, 1)}");
+                Console.WriteLine($"Деление выполнено: {Divide(array, 0, 3)}");
 			}
 			catch (DivideByZeroException zeroException)
 			{
@@ -31,6 +53,18 @@ namespace CSharp
 			{
                 Console.WriteLine(indexException.Message);
             }
+			catch (DivideException divideException)
+			{
+                Console.WriteLine(divideException.Message);
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+			finally
+			{
+                Console.WriteLine("Несмотря на все исключения деление отработало!");
+			}
         }
 	}
 }
@@ -51,4 +85,4 @@ namespace CSharp
 // TargetSite
 
 //		Практика 
-// Необходимо воспроизвести ошибки в той же самой программе оставшиеся блоки catch: IndexOutOfRangeException, DivideByZeroException
+// Написать класс обработки исключения, наследующийся от Exception, хранящий в себе поле с массивом и переопределяющий сообщение о том, что массив не проинициализирован.
