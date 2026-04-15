403d25a39d2d34c655367b03bdc3f850a1c5dc19
ChocoChocobo <sharonovjr@yahoo.com>
2026-04-15 14:48:06 +0300

    Абстрактные классы и интерфейсы

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -1,84 +1,51 @@
 ﻿using System;
 using System.Diagnostics;
 
-class Animal
+// Абстрактный класс животного, определяющий функционал только для классов одной природы, то есть животных!
+abstract class Animal
 {
-	public int paws = 4;
-	public string name = "Животный";
-
-	public Animal(int paws, string name)
-	{
-		this.paws = paws;
-		this.name = name;
-	}
-
-	public void Drink() => Console.WriteLine($"{name} пъёт!");
+    public abstract void Move();
+    public abstract void Eat();
 }
 
-// Синтаксис:
-// class имя_наследника : имя_базового_класса
-class Cat : Animal
+// Интерфейс, определяющий функционал для движимых объектов.
+// ОНИ МОГУТ БЫТЬ РАЗНОЙ ПРИРОДЫ!
+public interface IMovable 
 {
-	public int extraLives = 8;
-
-	// Инициализация родительского класса
-	public Cat(int extraLives, int paws, string name) : base(paws, name)
-	{
-		this.extraLives = extraLives;
-	}
-
-	public void Fall(int amount)
-	{
-        Console.WriteLine($"{name} упало на {amount} урона :(");
-		extraLives -= amount;
+    public void Move();
+    public void CollisionDetect() // пример-функция столкновения
+    {
+        Console.WriteLine("Объект столкнулся с другим объектом.");
     }
+}
+
+class ComputerMouse : IMovable
+{
+    public void Move() => Console.WriteLine("Мышь движется");
 }
 
 class Racoon : Animal
 {
-	public float garbageCollected = 0;
-
-	public Racoon(float garbageCollected, int paws) : base(paws, "ик Полоскун")
-	{
-		this.garbageCollected = garbageCollected;
-	}
-
-	public void CollectGarbage(float garbageCollected)
-	{
-		Console.WriteLine($"Енот{name} собрал {garbageCollected} кг мусора.");
-		this.garbageCollected += garbageCollected;
-	}
+    public override void Eat() => Console.WriteLine("Енотик ест");
+    public override void Move() => Console.WriteLine("Енотик ходит");
 }
 
 class Source
-{
-	static void Main(string[] args)
-	{
-		Cat cat = new Cat(8, 4, "Шарик");
-		Racoon racoon = new Racoon(0, 4);
+{    
+    static void Main(string[] args)
+    {
+        Racoon racoon = new Racoon();
+        racoon.Move();
+        racoon.Eat();
 
-		cat.Drink();
-		cat.Fall(1);
-
-		racoon.Drink();
-		racoon.CollectGarbage(1);
-
-		// Преобразование производного класса к базовому
-		Animal animal = cat;
-		animal.Drink();
-
-		// Операторы as и is являются операторами явного преобразования одного объекта класса в другой
-		Animal animal1 = racoon as Animal;
-		// Оператор is применяется для проверки на сущность объекта и используется в качестве условия
-		// Вернется true если объект имеет значения со сравниваемым типом данных и наоборот вернется false
-		if (racoon is Animal)
-		{
-			racoon.CollectGarbage(5);
-		}
-	}
+        ComputerMouse computerMouse = new ComputerMouse();
+        computerMouse.Move();
+    }
 }
 
-//		Практика
-// 1. Создать базовый класс типа музыкального инструмента, содержащий конструктор и 3 различных поля с разными типами данных, имеющие свойства get, set. Предусмотреть функцию взаимодействия с инструментом, которая бы подходила любому типу инструмента.
-// 2. Создать два производных класса от типа музыкального инструмента, содержащие 2 различных поля с разными типами данных, имеющие свойства get, set. Предусмотреть уникальную функцию для взаимодействия с данным типом инструмента. В конструкторе инициализировать базовый класс через base.
-// 3. Продемонстрировать функционал объектов и проверять, если созданный объект является, например, гитарой, выводить в сообщение в консоль.
+//      Практика
+// 1. Дополнить программу новым интерфейсом функционала клички. В интерфейсе должны быть поле имени с определенными get и set свойствами и функция, которая бы в консоль выводила кличку животного в консоли.
+// 2. В Main создать массив зоопарка, который должен хранить в себе объекты интерфейсов.
+// 3. Дополнить классы животных функцией Pet(), которая выводит в консоль о том, что пользователь гладит животное + имя
+// 4. Просить пользователя ввести имя животного, которое он хочет погладить.
+// 5. В цикле перебирать каждое животное и сравнивать его имя со вводом пользователя. Если есть совпадение, то вызывать функцию Pet() от перечисляемого объекта.
