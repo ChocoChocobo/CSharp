5828ddfbf7ea67ca2754bd5596de6a08b48d28bb
ChocoChocobo <sharonovjr@yahoo.com>
2026-04-23 14:56:20 +0300

    23.04 Введение в LINQ

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -2,61 +2,63 @@ using System;
 using System.Diagnostics;
 using System.Numerics;
 
+// Класс-ячейка коллекции
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public uint Score { get; set; }
+
+    public Student(int id, string name, uint score)
+    {
+        Id = id;
+        Name = name;
+        Score = score;
+    }
+}
+
+// Класс-агрегатор, который бы описывал объект, агрегирующий информацию из класса ячейки
+class StudentPerformance
+{
+    public string Name { get; set; }
+    public uint Score { get; set; }
+
+    public StudentPerformance(string name, uint score)
+    {
+        Name = name;
+        Score = score;
+    }
+}
+
 class Source
 {    
     static void Main(string[] args)
-    {
-        // Инициализация объекта очереди
-        // Queue<string> stringsQueue = new Queue<string>(); 
-        // Stack<int> intsStack = new Stack<int>();
-        // Аналогичным образом со списком эти коллекции инициализируются значениями.
-        /*Queue<string> stringsQueue = new Queue<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
-        // Функция Enqueue добавляет элемент в конец очереди.
-        stringsQueue.Enqueue("Родин");
-        // Функция Dequeue высвобождает элемент из очереди
-        stringsQueue.Dequeue();
-        // Функция, принимающая параметр вместимости и убеждающаяся, что текущая вместимость очереди меньше указанной. Если больше, то увеличивает вместимость вдвое.
-        Console.WriteLine(stringsQueue.Count);
-        Console.WriteLine(stringsQueue.EnsureCapacity(7));
-        foreach (string item in stringsQueue)
+    {        
+        // Вариант инициализации коллекции с пользовательским типом данных
+        List<Student> students = new List<Student>
+        {
+            new Student(1, "Андрей", 10),
+            new Student(2, "Давид", 1),
+            new Student(3, "Рия", 5),
+        };
+        // В переменную помещаем выборочные значения из коллекции в соответствии с условием
+        //            временный объект              обращение к полю
+        var names = from student in students select student.Name;
+        foreach (string item in names)
         {
             Console.WriteLine(item);
-        }*/
+        }
 
-        /*Stack<string> stringsStack = new Stack<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
-        stringsStack.Push("Родин"); // Функция, добавляющая элемент на верхушку стека
-        string poppedItem = stringsStack.Pop(); // Функция, убирающая и возврающая элемент стека с верхушки
-        string? tryPoppedItem = "";
-        
-        if (stringsStack.TryPop(out tryPoppedItem)) Console.WriteLine($"Элемент успешно изъятирован: {tryPoppedItem}");
-        else Console.WriteLine("Провал!");
-
-        Console.WriteLine($"Снятый элемент с верхушки стека: {poppedItem}");
-        foreach (string item in stringsStack)
+        var studentPerformances = from student in students select new StudentPerformance(student.Name, student.Score);
+        foreach (var item in studentPerformances)
         {
-            Console.WriteLine(item);
-        }*/
-
-        // Класс Dictionary представляет собой словарь, в котором имеется два шаблонных параметра: TKey - ключ и TValue - значение. Ключи уникальны, так что при вставке новой пары с ранее не существовавшим ключом появится новая пара. При вставке пары с ранее существовавшим ключом, у пары обновится значение.
-        // Одна пара в словаре определяется объектом класса KeyValuePair по аналогии с тем, как это работает в связном списке. У данного объекта имеются свойства Key и Value
-        Dictionary<string, uint> studentsDictionary = new Dictionary<string, uint>();
-        // С помощью функции Add можно вставить элемент, передав ключ, а затем значение
-        studentsDictionary.Add("Давид", 2);
-        studentsDictionary.Add("Рия", 5);
-        studentsDictionary.Add("Лев", 2);
-        studentsDictionary.Add("Тимур", 16);
-        studentsDictionary.Add("Филипп", 21);
-        studentsDictionary.Add("Расул", 7);
-        studentsDictionary.Add("Андрей", 0);
-        studentsDictionary.Add("Тимур Тимурович", 67);
-        // Альтернативно можно добавлять элементы с помощью оператора квадратных скобок, указывая ключ, а через присваивание - значение
-        studentsDictionary["Родин"] = 666;
-
-        foreach (KeyValuePair<string, uint> item in studentsDictionary)
-        {
-            Console.WriteLine($"Студент {item.Key} с оценкой {item.Value}");
+            Console.WriteLine($"Студент {item.Name} с оценкой {item.Score}");
         }
     }
 }
 
-//      Очереди, стеки и словари
+//      Практика
+// 1. Создать элементарный класс животого, у которого есть поля имени, цвета, кол-ва ног
+// 2. Создать класс-агрегатор, у которого есть поля имени и кол-ва ног
+// 3. В Main создать список животных в количестве 5 объектов и с помощью LINQ-выражения в объект животных выбрать объекты класса-агрегатора
+// 4. Создать две очереди четырехногих и двуногих животных. При перечислении в цикле foreach всех объектов класса-агрегатора, сравнивать с полем кол-ва ног и помещать животных в очереди.
