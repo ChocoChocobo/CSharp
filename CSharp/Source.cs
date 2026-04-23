939731f969e810f6d3d25468b0bbecde4df164d3
ChocoChocobo <sharonovjr@yahoo.com>
2026-04-23 14:22:55 +0300

    23.04 Коллекции

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -6,62 +6,57 @@ class Source
 {    
     static void Main(string[] args)
     {
-        // 1. Инициализация пустого списка 
-        List<int> intList = new List<int>();
-        // 2. Инциализация списка с 5 объектами
-        List<int> intList1 = new List<int>(5);
-        // 3. Инициализация списка без фиксированного количества элементов
-        List<int> intList2 = new List<int>() { 1, 2, 3, 4, 5};
-        // 4. Инициализация списка операцией копирования другого списка
-        List<int> intList3 = new List<int>(intList2);
-
-        List<string> fruits = new List<string>();
-        fruits.Add("винорез"); // добавление элемента в конец списка
-        fruits.AddRange(new[] { "огуград", "редиска" }); // добавление диапазона значений
-        fruits.Insert(2, "ромовин"); // вставка значения по индексу
-        fruits.InsertRange(1, new[] { "аксидер", "даргуго" }); // вставка диапазона значений по индексу
-        
-        //Console.WriteLine(fruits.Contains("редиска")); // проверяет наличие содержимого
-        //Console.WriteLine(fruits.Exists(fruit => fruit[0] == 'з')); // проверка наличия подходящего элемента по заданному условию. Применяем лямбду для быстрой и удобной записи функции по сравнению нулеового значения по индексу у фрукта
-        //Console.WriteLine(fruits.Find(fruit => fruit[0] == 'р')); // поиск первого элемента по условию и возвращение его
-        List<string> fruits_all = fruits.FindAll(fruit => fruit[0] == 'р'); // возвращение набора элементов которые удовлетворяют заданному условию
-        
-        /*foreach (string fruit in fruits_all)
+        // Инициализация объекта очереди
+        // Queue<string> stringsQueue = new Queue<string>(); 
+        // Stack<int> intsStack = new Stack<int>();
+        // Аналогичным образом со списком эти коллекции инициализируются значениями.
+        /*Queue<string> stringsQueue = new Queue<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
+        // Функция Enqueue добавляет элемент в конец очереди.
+        stringsQueue.Enqueue("Родин");
+        // Функция Dequeue высвобождает элемент из очереди
+        stringsQueue.Dequeue();
+        // Функция, принимающая параметр вместимости и убеждающаяся, что текущая вместимость очереди меньше указанной. Если больше, то увеличивает вместимость вдвое.
+        Console.WriteLine(stringsQueue.Count);
+        Console.WriteLine(stringsQueue.EnsureCapacity(7));
+        foreach (string item in stringsQueue)
         {
-            Console.WriteLine(fruit);
+            Console.WriteLine(item);
         }*/
 
-        // LinkedList
-        // Инициализация связного списка значениями из списка List
-        LinkedList<string> linkedFruits = new LinkedList<string>(fruits);
-        // В отдельный объект заносим первый узел, для последующего перебора и вывода остальных узлов
-        LinkedListNode<string>? currentNode = linkedFruits.First;
-        Console.WriteLine("------------");
-        while (currentNode != null)
+        /*Stack<string> stringsStack = new Stack<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
+        stringsStack.Push("Родин"); // Функция, добавляющая элемент на верхушку стека
+        string poppedItem = stringsStack.Pop(); // Функция, убирающая и возврающая элемент стека с верхушки
+        string? tryPoppedItem = "";
+        
+        if (stringsStack.TryPop(out tryPoppedItem)) Console.WriteLine($"Элемент успешно изъятирован: {tryPoppedItem}");
+        else Console.WriteLine("Провал!");
+
+        Console.WriteLine($"Снятый элемент с верхушки стека: {poppedItem}");
+        foreach (string item in stringsStack)
         {
-            Console.WriteLine(currentNode.Value);
-            currentNode = currentNode.Next;
+            Console.WriteLine(item);
+        }*/
+
+        // Класс Dictionary представляет собой словарь, в котором имеется два шаблонных параметра: TKey - ключ и TValue - значение. Ключи уникальны, так что при вставке новой пары с ранее не существовавшим ключом появится новая пара. При вставке пары с ранее существовавшим ключом, у пары обновится значение.
+        // Одна пара в словаре определяется объектом класса KeyValuePair по аналогии с тем, как это работает в связном списке. У данного объекта имеются свойства Key и Value
+        Dictionary<string, uint> studentsDictionary = new Dictionary<string, uint>();
+        // С помощью функции Add можно вставить элемент, передав ключ, а затем значение
+        studentsDictionary.Add("Давид", 2);
+        studentsDictionary.Add("Рия", 5);
+        studentsDictionary.Add("Лев", 2);
+        studentsDictionary.Add("Тимур", 16);
+        studentsDictionary.Add("Филипп", 21);
+        studentsDictionary.Add("Расул", 7);
+        studentsDictionary.Add("Андрей", 0);
+        studentsDictionary.Add("Тимур Тимурович", 67);
+        // Альтернативно можно добавлять элементы с помощью оператора квадратных скобок, указывая ключ, а через присваивание - значение
+        studentsDictionary["Родин"] = 666;
+
+        foreach (KeyValuePair<string, uint> item in studentsDictionary)
+        {
+            Console.WriteLine($"Студент {item.Key} с оценкой {item.Value}");
         }
-        Console.WriteLine("------------");
-        foreach (string fruit in fruits_all)
-        {
-            Console.WriteLine(fruit);
-        }
-        fruits.Clear();
-        Console.WriteLine("------------");
-        currentNode = linkedFruits.First;
-        while (currentNode != null)
-        {
-            Console.WriteLine(currentNode.Value);
-            currentNode = currentNode.Next;
-        }        
     }
 }
 
-//      Коллекции
-// В языке C# имеются готовые реализации абстрактных структур данных.
-// Список (List) - аналог контейнера (массива) из языка С++. Такая готовая структура данных позволяет пользоваться удобными функциями по добавлению, удалению, сортировке, обращению по индексу и так далее.
-// Связный список (LinkedList) - двусвязный линейный список. Такой список состоит из узлов, то есть объектов класса LinkedListNode<T>. У этого класса есть свойства:
-// - Next - возвращает ссылку на следующий узел. Если следующего узла нет, то возвращает null
-// - Previous - возвращает ссылку на предыдущий узел. Если предыдущего узла нет, то возвращает null
-// - Value - возвращает или устанавливает значение, хранящееся в узле
+//      Очереди, стеки и словари
