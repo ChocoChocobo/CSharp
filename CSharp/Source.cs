2287dd410619beb9c05bab9730de54889ea8150c
ChocoChocobo <sharonovjr@yahoo.com>
2026-05-26 13:43:21 +0300

    26.05 Введение нулевого объекта

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -2,93 +2,79 @@ using System;
 using System.Diagnostics;
 using System.Numerics;
 
+// Интерфейс, определяющий поля пиццы
+public interface IPizza
+{
+    string Name { get; }
+    string Topping { get; }
+    double Price { get; }
+    bool IsEatable { get; }
+    bool IsNull { get; }
+}
+
+// Конкретный класс пиццы
+public class AnanasPizza : IPizza
+{
+    public string? Name { get; set; }
+    public string? Topping { get; set; }
+    public double Price { get; set; }
+    public bool IsEatable { get; set; }
+
+    // Поле определяющее нулевой ли объект
+    public bool IsNull => false;
+
+    public AnanasPizza(string name, string topping, double price, bool isEatable) 
+    {
+        Name = name;
+        Topping = topping;
+        Price = price;
+        IsEatable = isEatable;
+    }    
+}
+
+// "Нулевой" класс пиццы, обозначающий поведение по умолчанию
+public class NullAnanasPizza : IPizza
+{
+    // Задаем нулевому объекту класса значения по умолчанию
+    public string Name => "Я пицца";
+    public string Topping => "Без начинки";
+    public double Price => 0.0;
+    public bool IsEatable => false;
+
+    // Поле определяющее нулевой ли объект
+    public bool IsNull => true;
+    
+}
+
+public class Order
+{
+    public string Name { get; set; }
+    public IPizza Pizza { get; set; }
+
+    public Order(string name, IPizza? pizza) 
+    {
+        Name = name;
+
+        // Поле пиццы у заказа инициализурется либо задаваемой пиццей, если не ноль, либо создается объект нулевой   
+        Pizza = pizza ?? new NullAnanasPizza();
+    }
+}
+
 class Source
 {    
     static void Main(string[] args)
     {
-        //      ТЕМА: ПАРАЛЛЕЛЬНОЕ И АСИНХРОННОЕ ПРОГРАММИРОВАНИЕ В C#
-        // Параллельное программирование - это выполнение двух или нескольких процессов одновременно. Асинхронное - это выполнение нескольких процессов параллельно друг другу, когда один или оба могут заниматься совершенно разными задачами.
-        // В языке C# основым средством для параллельного выполнения нескольких задач является библиотека TPL (Task Parallel Library).
-        // Объект класса Task принимает в качестве параметра блок кода (функцию). Для несложного кода можно использовать лямбда-выражение.
+        Order order = new Order("Заказ 0", null);
+        Console.WriteLine(order.Pizza.Name);
 
-        // Первый способ создания задачи
-        // Task task1 = new Task(PrintInfo);
-        // task1.Start();
-        // Второй способ создания задачи
-        // Task task2 = Task.Factory.StartNew(PrintInfo);
-        // Третий способ создания задачи
-        // Task task3 = Task.Run(PrintInfo);
-
-        // После запуска задачи этот метод будет выполняться в отдельном потоке. Поскольку данный метод, в котором были запущены задачи, выполняется в своем потоке, он может завершить работу раньше, чем основной поток. Для того, чтобы дождаться выполнения задачи, можно использовать функцию Wait():
-        // task3.Wait();
-
-        // Может возникнуть ситуация, при которой очередь задач определяется "самовольно", несмотря на правильную последовательность написанного кода. Это происходит из-за выставления планировщиком задач своих приоритетов для запускаемых задач.
-        /*Task task1 = Task.Run(DoTask1);
-        Task task2 = Task.Run(DoTask2);
-        Task task3 = Task.Run(DoTask3);
-        Task.WaitAll(task1, task2, task3);*/
-        /*task1.Wait();
-        task2.Wait();
-        task3.Wait();*/
-
-        // Задача может запускать другую - вложенную. При этом даже если метод, запустивший внешнюю задачу, ждет окончания выполнения внешней задачи...
-        // Чтобы внутренняя задача была помечена, как внутренняя, при ее создании нужно указать параметр:
-        Task task1 = Task.Run(DoTask1);
-        Task task2 = Task.Run(DoTask2);
-        Task task3 = Task.Run(DoTask3);
-        Task innerTask2 = new Task(DoInnerTask2, TaskCreationOptions.AttachedToParent);
-        innerTask2.Start();
-
-        Task.WaitAll(task1, task2, task3);
-
-        // Возвращение результата. Для этого применяются шаблонные объекты класса Task
-        Task<int> taskIntReturn = new Task<int>(() => 3 + 3);
-        taskIntReturn.Start();
-        int taskIntResult = taskIntReturn.Result;
-        Console.WriteLine($"Реузльтат работы задачи по складывания значений: {taskIntResult}");
-
-        // Задачу можно продолжить после завершения, передав управление другому объекту:
-        Task taskContinue = taskIntReturn.ContinueWith(PrintResult);
-    }
-
-    public static void PrintResult(Task<int> task)
-    {
-        Console.WriteLine($"Результат задачи: {task.Result}");
-    }
-
-    public static void DoTask1()
-    {
-        Console.WriteLine("Начинаю процесс 1.");
-        Thread.Sleep(1000);
-        Console.WriteLine("Заканчиваю процесс 1.");
-    }
-
-    public static void DoTask2()
-    {
-        Console.WriteLine("Начинаю процесс 2.");
-        Thread.Sleep(10000);
-        Console.WriteLine("Заканчиваю процесс 2.");
-    }
-
-    public static void DoTask3()
-    {
-        Console.WriteLine("Начинаю процесс 3.");
-        Thread.Sleep(5000);
-        Console.WriteLine("Заканчиваю процесс 3.");
-    }
-
-    public static void DoInnerTask2()
-    {
-        Console.WriteLine("Начинаю внутренний процесс у Task 2.");
-        Thread.Sleep(3000);
-        Console.WriteLine("Заканчиваю внутренний процесс у Task 2.");
-        
-    }
+        Order order1 = new Order("Заказ 1", new AnanasPizza("Давид", "В ананасе", 1.0, false));
+        Console.WriteLine(order1.Pizza.Name);
+    }    
 }
 
-//      Практика - написать калькулятор, который высчитывает результат в отдельных задачах Task. Калькулятор включает в себя операции: +, -, /, *, %, MathF.Sqrt (квадрат), MathF.Log (логарифм), MathF.Pow (возведение в степень).
-// 0. Программа должна работать в цикле do while, пока пользователь не захочет выйти.
-// 1. Написать небольшое меню-обработку комманд пользователя через конструкцию switch;
-// 2. В каждом case запускать отдельную задачу в зависимости от выбранного действия пользователем.
-// 3. После выполнения задачи нужно считывать результат и печатать в консоль пользователю.
-// Примечание: использовать Task, математические функции из класса MathF
+//      Практика
+// Продемонстрировать введение нулевого объекта на примере наследования от абстрактного класса:
+// 1. Создать абстрактный класс пиццы с полями названия, стоимости и начинки;
+// 2. Создать наследуемый конкретный класс пиццы;
+// 3. Создать класс заказа и продемонстрировать работу с оператором ?? при инициализации поля заказа;
+// 4. В Main создать пару объектов с нулем и с настоящим объектом.
