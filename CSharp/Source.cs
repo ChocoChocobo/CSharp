a2d832ac94da6b14b27120841a6a77d093bd1e1a
ChocoChocobo <sharonovjr@yahoo.com>
2026-05-27 13:38:18 +0300

    27.05  GarbageCollector

--- CSharp/Source.cs
+++ CSharp/Source.cs
@@ -2,79 +2,85 @@ using System;
 using System.Diagnostics;
 using System.Numerics;
 
-// Интерфейс, определяющий поля пиццы
-public interface IPizza
-{
-    string Name { get; }
-    string Topping { get; }
-    double Price { get; }
-    bool IsEatable { get; }
-    bool IsNull { get; }
-}
-
-// Конкретный класс пиццы
-public class AnanasPizza : IPizza
-{
-    public string? Name { get; set; }
-    public string? Topping { get; set; }
-    public double Price { get; set; }
-    public bool IsEatable { get; set; }
-
-    // Поле определяющее нулевой ли объект
-    public bool IsNull => false;
-
-    public AnanasPizza(string name, string topping, double price, bool isEatable) 
-    {
-        Name = name;
-        Topping = topping;
-        Price = price;
-        IsEatable = isEatable;
-    }    
-}
-
-// "Нулевой" класс пиццы, обозначающий поведение по умолчанию
-public class NullAnanasPizza : IPizza
-{
-    // Задаем нулевому объекту класса значения по умолчанию
-    public string Name => "Я пицца";
-    public string Topping => "Без начинки";
-    public double Price => 0.0;
-    public bool IsEatable => false;
-
-    // Поле определяющее нулевой ли объект
-    public bool IsNull => true;
-    
-}
-
-public class Order
-{
-    public string Name { get; set; }
-    public IPizza Pizza { get; set; }
-
-    public Order(string name, IPizza? pizza) 
-    {
-        Name = name;
-
-        // Поле пиццы у заказа инициализурется либо задаваемой пиццей, если не ноль, либо создается объект нулевой   
-        Pizza = pizza ?? new NullAnanasPizza();
-    }
-}
-
 class Source
 {    
+    class RubberDuck : IDisposable // Интерфейс по самостоятельному применению очистки мусора для неуправляемых объектов
+    {
+        public string Name { get; set; } = "Давид";
+
+        private bool disposed = false;
+
+        // Метод интерфейса IDisposable
+        public void Dispose()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+            Console.WriteLine($"Жизнь объекта уточки подошла к концу в Dispose...");
+        }
+
+        // Метод, переопределяющий Dispose, который отталкивается от флага dispsoed (очищен ли объект)
+        // Данный метод нужен для того, чтобы очистить управляемые объекты. После того, как они будут очищены, флаг disposed становится true и происходит очистка неуправляемая
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                // Высвобождаем ресурс
+            }
+            disposed = true;
+        }
+
+        // Неуправляемым кодом занимается разработчик, применяя деструктор или интерфейс IDisposable. К неуправляемому коду относятся API, которыми сборщик мусора не знает как распоряжаться.
+        // Деструктор на самом деле это замаскированный метод Finalize(). Компилятор преобразует деструктор в следующую конструкцию:
+        /*protected override void Finalize()
+        {
+            try
+            {
+
+            }
+            finally
+            {
+                base.Finalize();
+            }
+        }*/
+
+        // Деструктор:
+        ~RubberDuck()
+        {
+            // Вопрос 1: Почему данное сообщение не отображается в консоли?
+            // Вопрос 2: Когда применять Dispose или деструктор в классе? В чем сущность комбинированного подхода?
+            Dispose(false);
+            Console.WriteLine("Жизнь объекта уточки подошла к концу в деструкторе...");
+        }
+    }
+    static void SayHi()
+    {
+        RubberDuck rubberDuck = new RubberDuck();
+        rubberDuck.Name = "ПУтя";
+        Console.WriteLine(rubberDuck.Name);
+        rubberDuck.Name = "Утятя";
+        
+        // Для явной очистки памяти у объекта можно напрямую вызвать функцию Dispose()
+        rubberDuck.Dispose();
+    }
+
     static void Main(string[] args)
     {
-        Order order = new Order("Заказ 0", null);
-        Console.WriteLine(order.Pizza.Name);
+        // Сборщик мусора в C# представлен классом GC
 
-        Order order1 = new Order("Заказ 1", new AnanasPizza("Давид", "В ананасе", 1.0, false));
-        Console.WriteLine(order1.Pizza.Name);
+        SayHi();
+        // Получение памяти, выделенной под кучу в байт
+        long memoryInfo = GC.GetTotalMemory(false);
+        // Заставить сборщик мусора проивести сборку в соответствии с одним из трех режимов
+        GC.Collect(0, GCCollectionMode.Optimized);
+        // Функция, приостанавливающая процесс до конца выполнения сборки мусора
+        GC.WaitForPendingFinalizers();
+        // Функция, позволяющая получить поколение объекта
+        int memoryInfoGen = GC.GetGeneration(memoryInfo);
+
+        Console.WriteLine($"Поколение для переменной memoryInfo: {memoryInfoGen}");
+        Console.WriteLine(memoryInfo);
+
+        Console.ReadKey();
     }    
 }
-
-//      Практика
-// Продемонстрировать введение нулевого объекта на примере наследования от абстрактного класса:
-// 1. Создать абстрактный класс пиццы с полями названия, стоимости и начинки;
-// 2. Создать наследуемый конкретный класс пиццы;
-// 3. Создать класс заказа и продемонстрировать работу с оператором ?? при инициализации поля заказа;
-// 4. В Main создать пару объектов с нулем и с настоящим объектом.
